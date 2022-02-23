e2858c96257b4fe1c0d025475ff9204ea780c95b
08YESHWANTH08 <99631735+08YESHWANTH08@users.noreply.github.com>
2022-02-23 17:22:18 +0530

    Update Task-4-Q-3.py

--- Task-4/Task-4-Q-3/Task-4-Q-3.py
+++ Task-4/Task-4-Q-3/Task-4-Q-3.py
@@ -1,4 +1,6 @@
+from tabulate import tabulate
 list1=[["Roll No","Name","Marks"]] #Yeshwanth A CH.EN.U4CSE21263
+l=[]
 cond='Y'
 dup=1
 while (cond.lower()=='y'):
@@ -7,23 +9,16 @@ while (cond.lower()=='y'):
     if choice==1:
         list1.append([int(input("Enter The Roll No.:")),input("Enter The Student Name:"),int(input("Enter The Marks:"))])
     elif choice==2:
-        for i in range(len(list1)):
-            for j in range(len(list1[i])):
-                print(list1[i][j], "\t\t\t", end="")
-            print("\n")
+        print(tabulate(list1,headers='firstrow',tablefmt='grid'))
     elif choice==3:
+
         element = int(input("Enter The Roll No:"))
         for i in range(len(list1)):
             if element == list1[i][0]:
-                for j in range(len(list1[i])):
-                    print(list1[0][j], "\t", end="")
-                print("\n")
-                for j in range(len(list1[i])):
-                    print(list1[i][j], "\t\t\t", end="")
-                print("\n")
-            else:
-                dup=0
-        if dup==0:
+                l=[list1[i]]
+                print(l)
+                print(tabulate(l,headers=["Roll No","Name","Marks"],tablefmt='grid'))
+        if l==[]:
             print("No Data Found")
     else:
         print("Enter A Valid Option\n")
