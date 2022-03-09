93469b4dce3ddcd9ca3dfb07ef8de466f77315e3
08YESHWANTH08 <99631735+08YESHWANTH08@users.noreply.github.com>
2022-03-09 17:56:51 +0530

    Delete Task-8 directory

--- Task-8/Task-8-4.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-result=''
-statement=input("Enter The Array (Separated By \' \'):").split()
-statement=np.array(statement)
-s_statement=pd.Series(statement)
-for i in range(len(statement)):
-    result+=(" "+s_statement[i])
-print(result.title())
